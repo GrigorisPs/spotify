9c6f25e1c0a5082744633189c2d2919330980818
GrigorisPs <grigorisg3jobs@hotmail.com>
2024-03-08 16:17:47 +0200

    created Home and search page

---
+++ src/components/CategorySelect.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref,toRefs } from 'vue';
+import { RouterLink, } from 'vue-router'; 
+import uniqolor from 'uniqolor';
+import { stringify } from 'postcss';
+
+let randColor=ref('')
+randColor.value=uniqolor.random();
+
+const props= defineProps({
+    category:String,
+    image:String
+})
+const {category} =toRefs(props);
+
+</script>
+
+<template>
+    <RouterLink to="library">
+
+        <div v-if="randColor.color"
+        :style="`background-color: ${randColor.color}`"
+        class="
+        contrast-[2]
+        brightness-[0.87]
+        rounded-xl
+        aspect-square
+        relative
+        overflow-hidden
+        "
+        >
+        <div class="text-white pt-3 pl-4 font-semibold text-[23px]">{{ category }}</div>
+        <img 
+            class=
+            "
+            absolute
+            contrast-[0.5]
+            -right-3.5
+            bottom-0
+            max-w-[100px]
+            rotate-[20deg]
+            "
+            :src="image || ''"
+            >
+
+
+        </div>
+    </RouterLink>
+</template>
+
+
